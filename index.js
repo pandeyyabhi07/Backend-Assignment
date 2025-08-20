cf4749441ba1ca6eddb60fd02768d1037a154e6b
Pandeyyabhi07 <pandeyyabhi001@gmail.com>
2025-08-20 18:48:21 +0530

    added

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/", (req, res) => {
+  res.send("API is running...");
+});
+
 app.use("/api/allowances", allowanceRoutes);
 app.use("/api/users", userRoutes);
 
